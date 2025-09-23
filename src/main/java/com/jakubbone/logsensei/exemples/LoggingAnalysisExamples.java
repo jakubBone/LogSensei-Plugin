0e4d9b9a22d34721f1158b78ad5959e2db78f187
jakubBone <jakub.bone1990@gmail.com>
2025-09-23 09:48:34 +0200

    feat: add special class for PSI structure analysis and logging pattern testing

---
+++ src/main/java/com/jakubbone/logsensei/exemples/LoggingAnalysisExamples.java
@@ -0,0 +1,20 @@
+package com.jakubbone.logsensei.exemples;
+
+/**
+ * Java code examples for PSI analysis and logging pattern testing.
+ *
+ * This class contains all the scenarios that LogSensei should detect and analyze.
+ * Each method represents a different test case from the plugin requirements.
+ * PSI Viewer analyse them  to understand the PSI structure of each case.
+ */
+public class LoggingAnalysisExamples {
+
+    // Expected: LogSensei should suggest adding log.error()
+    public void emptyCatchBlock() {
+        try {
+            int result = 10 / 0;
+        } catch (Exception e) {
+            // Empty catch
+        }
+    }
+}
