103e201e8172f6df86ae948bf0ed8a54e4f74cf8
jakubBone <jakub.bone1990@gmail.com>
2025-10-13 11:38:50 +0200

    feat: implemented LocalQuickFix interface to LoopLogQuickFix

--- src/main/java/com/jakubbone/logsensei/quickfix/LoopLogQuickFix.java
+++ src/main/java/com/jakubbone/logsensei/quickfix/LoopLogQuickFix.java
@@ -1,4 +1,19 @@
 package com.jakubbone.logsensei.quickfix;
 
-public class LoopLogQuickFix {
+import com.intellij.codeInspection.LocalQuickFix;
+import com.intellij.codeInspection.ProblemDescriptor;
+import com.intellij.codeInspection.util.IntentionFamilyName;
+import com.intellij.openapi.project.Project;
+import org.jetbrains.annotations.NotNull;
+
+public class LoopLogQuickFix implements LocalQuickFix  {
+    @Override
+    public @IntentionFamilyName @NotNull String getFamilyName() {
+        return "";
+    }
+
+    @Override
+    public void applyFix(@NotNull Project project, @NotNull ProblemDescriptor problemDescriptor) {
+
+    }
 }
