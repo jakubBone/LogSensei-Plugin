3b61a2fcb8b496e09c271fc639faa2384b9b0ac6
jakubBone <jakub.bone1990@gmail.com>
2025-10-15 12:54:56 +0200

    feat: add EarlyReturnInspection with initial buildVisitor logic

--- src/main/java/com/jakubbone/logsensei/inspection/EarlyReturnInspection.java
+++ src/main/java/com/jakubbone/logsensei/inspection/EarlyReturnInspection.java
@@ -3,14 +3,45 @@ package com.jakubbone.logsensei.inspection;
 import com.intellij.codeInspection.AbstractBaseJavaLocalInspectionTool;
 import com.intellij.codeInspection.ProblemsHolder;
 import com.intellij.psi.JavaElementVisitor;
+import com.intellij.psi.PsiCodeBlock;
 import com.intellij.psi.PsiElementVisitor;
+import com.intellij.psi.PsiExpression;
+import com.intellij.psi.PsiMethod;
+import com.intellij.psi.PsiReturnStatement;
+import com.intellij.psi.util.PsiTreeUtil;
 import org.jetbrains.annotations.NotNull;
 
 public class EarlyReturnInspection extends AbstractBaseJavaLocalInspectionTool {
+
     @Override
     public @NotNull PsiElementVisitor buildVisitor(@NotNull ProblemsHolder holder, boolean isOnTheFly) {
         return new JavaElementVisitor(){
+            @Override
+            public void visitReturnStatement(@NotNull PsiReturnStatement statement) {
+                super.visitReturnStatement(statement);
+                PsiExpression returnValue = statement.getReturnValue();
 
+                if(returnValue != null){
+                    return;
+                }
+
+                PsiMethod method = PsiTreeUtil.getParentOfType(statement, PsiMethod.class);
+                if(method == null){
+                    return;
+                }
+
+                PsiCodeBlock body = method.getBody();
+                if(body == null){
+                    return;
+                }
+
+                // isLastStatement
+                // hasLogBeforeReturn()
+                // isPrintBeforeMethod()
+
+                // registerProblem()
+
+            }
         };
     }
 }
