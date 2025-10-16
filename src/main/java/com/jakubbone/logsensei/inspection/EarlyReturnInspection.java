5de4182d2802984fc8224a930c7f87e6cb3245c5
jakubBone <jakub.bone1990@gmail.com>
2025-10-16 15:30:49 +0200

    feat: implement last return check method in EarlyReturnInspection

--- src/main/java/com/jakubbone/logsensei/inspection/EarlyReturnInspection.java
+++ src/main/java/com/jakubbone/logsensei/inspection/EarlyReturnInspection.java
@@ -4,10 +4,12 @@ import com.intellij.codeInspection.AbstractBaseJavaLocalInspectionTool;
 import com.intellij.codeInspection.ProblemsHolder;
 import com.intellij.psi.JavaElementVisitor;
 import com.intellij.psi.PsiCodeBlock;
+import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiElementVisitor;
 import com.intellij.psi.PsiExpression;
 import com.intellij.psi.PsiMethod;
 import com.intellij.psi.PsiReturnStatement;
+import com.intellij.psi.PsiStatement;
 import com.intellij.psi.util.PsiTreeUtil;
 import org.jetbrains.annotations.NotNull;
 
@@ -35,13 +37,31 @@ public class EarlyReturnInspection extends AbstractBaseJavaLocalInspectionTool {
                     return;
                 }
 
-                // isLastStatement
+                if(isLastStatement(statement, body)){
+                    return;
+                }
+
                 // hasLogBeforeReturn()
-                // isPrintBeforeMethod()
-
+                // hasPrintBeforeMethod()
                 // registerProblem()
-
             }
         };
+    }
+
+    private boolean isLastStatement(PsiReturnStatement returnStmt, PsiCodeBlock methodBody){
+        PsiStatement[] statements = methodBody.getStatements();
+        if(statements.length == 0){
+            return true;
+        }
+        PsiStatement lastStmt = statements[statements.length - 1];
+        return returnStmt.equals(lastStmt);
+    }
+
+    private void hasLogBeforeReturn(){
+
+    }
+
+    private void hasPrintBeforeMethod(){
+
     }
 }
