cef75852ef8abea2577c70ac2ea8f2cb44358450
jakubBone <jakub.bone1990@gmail.com>
2025-11-02 15:09:09 +0100

    feat: implement first log check in public methods in Service inspection class

--- src/main/java/com/jakubbone/logsensei/inspection/ServiceMethodInspection.java
+++ src/main/java/com/jakubbone/logsensei/inspection/ServiceMethodInspection.java
@@ -10,6 +10,7 @@ import com.intellij.psi.PsiCodeBlock;
 import com.intellij.psi.PsiElementVisitor;
 import com.intellij.psi.PsiMethod;
 import com.intellij.psi.PsiModifierList;
+import com.intellij.psi.PsiStatement;
 import com.intellij.psi.util.PsiTreeUtil;
 import org.jetbrains.annotations.NotNull;
 
@@ -40,10 +41,29 @@ public class ServiceMethodInspection extends AbstractBaseJavaLocalInspectionTool
                     return;
                 }
 
-                return;
+                if(isFirstStatementLog(body)){
+
+                }
+
 
             }
         };
+    }
+
+    private boolean isFirstStatementLog(PsiCodeBlock methodBody){
+        PsiStatement[] statements = methodBody.getStatements();
+        if(statements.length == 0){
+            return true;
+        }
+        PsiStatement firstStmt = statements[0];
+
+        String text = firstStmt.getText();
+        if(text.contains("log.") ||
+                text.contains("logger.") ||
+                text.contains("System.out.print")){
+            return true;
+        }
+        return false;
     }
 
 }
