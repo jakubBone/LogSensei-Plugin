f70c1c51f5728c78dbf87f59d8dd721079240d31
jakubBone <jakub.bone1990@gmail.com>
2025-11-05 13:06:40 +0100

    feat: implement addEntryLog method to service quick fix

--- src/main/java/com/jakubbone/logsensei/quickfix/ServiceMethodLogQuickFix.java
+++ src/main/java/com/jakubbone/logsensei/quickfix/ServiceMethodLogQuickFix.java
@@ -1,15 +1,23 @@
 package com.jakubbone.logsensei.quickfix;
 
+import static com.jakubbone.logsensei.utils.LogSenseiConstants.LOG_PATTERN_DEBUG;
+import static com.jakubbone.logsensei.utils.LogSenseiConstants.LOG_PATTERN_SERVICE_ENTRY_INFO;
+import static com.jakubbone.logsensei.utils.LogSenseiConstants.LOG_PATTERN_SERVICE_EXIT_INFO;
+import static com.jakubbone.logsensei.utils.LogSenseiConstants.LOG_PATTERN_SERVICE_INFO;
 import static com.jakubbone.logsensei.utils.LogSenseiUtils.addLog4jAnnotationAndImports;
 
 import com.intellij.codeInspection.LocalQuickFix;
 import com.intellij.codeInspection.ProblemDescriptor;
 import com.intellij.codeInspection.util.IntentionFamilyName;
 import com.intellij.openapi.project.Project;
+import com.intellij.psi.JavaPsiFacade;
 import com.intellij.psi.PsiClass;
+import com.intellij.psi.PsiCodeBlock;
 import com.intellij.psi.PsiElement;
 
+import com.intellij.psi.PsiElementFactory;
 import com.intellij.psi.PsiMethod;
+import com.intellij.psi.PsiStatement;
 import com.intellij.psi.util.PsiTreeUtil;
 import org.jetbrains.annotations.NotNull;
 
@@ -24,7 +32,13 @@ public class ServiceMethodLogQuickFix implements LocalQuickFix {
 
     @Override
     public @IntentionFamilyName @NotNull String getFamilyName() {
-        return "Add INFO log to service method";
+        if (needsEntryLog && needsExitLog) {
+            return "Add entry and exit INFO logs";
+        } else if (needsEntryLog) {
+            return "Add entry INFO log";
+        } else {
+            return "Add exit INFO log";
+        }
     }
 
     @Override
@@ -44,7 +58,43 @@ public class ServiceMethodLogQuickFix implements LocalQuickFix {
             return;
         }
 
+        if(needsEntryLog){
+            addEntryLog(project, psiMethod);
+        }
+
+        if(needsExitLog){
+            addExitLog(project, psiMethod);
+        }
+
         addLog4jAnnotationAndImports(project, containingClass);
+
+    }
+
+    private void addEntryLog(Project project, PsiMethod method){
+        PsiCodeBlock body = method.getBody();
+        if(body == null){
+            return;;
+        }
+
+        // Create log statement
+        String logStatementText = String.format(
+                LOG_PATTERN_SERVICE_ENTRY_INFO,
+                method.getName()
+        );
+
+        PsiElementFactory factory = JavaPsiFacade.getElementFactory(project);
+        PsiStatement logStatement = factory.createStatementFromText(logStatementText, method);
+
+        PsiStatement [] statements = body.getStatements();
+
+        if(statements.length > 0){
+            body.addBefore(logStatement, statements[0]);
+        } else {
+            body.add(logStatement);
+        }
+    }
+
+    private void addExitLog(Project project, PsiMethod method){
 
     }
 }
