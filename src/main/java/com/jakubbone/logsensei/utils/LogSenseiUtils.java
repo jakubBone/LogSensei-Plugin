b87090a5c6d697e9ff2a9e3cd63ad45a3f462d7f
jakubBone <jakub.bone1990@gmail.com>
2025-11-17 11:21:32 +0100

    fix: Log4j2 FQN '@lombok.extern.log4j.Log4j2' annotation instead raw '@Log4j2'

--- src/main/java/com/jakubbone/logsensei/utils/LogSenseiUtils.java
+++ src/main/java/com/jakubbone/logsensei/utils/LogSenseiUtils.java
@@ -10,20 +10,32 @@ import com.intellij.psi.codeStyle.JavaCodeStyleManager;
 import org.jetbrains.annotations.NotNull;
 
 public class LogSenseiUtils {
-    private static final String LOMBOK_LOG4J2 = "Log4j2";
+    private static final String LOMBOK_LOG4J2_FQN = "lombok.extern.log4j.Log4j2";
 
     public static void addLog4jAnnotationAndImports(@NotNull Project project, @NotNull PsiClass psiClass){
         PsiModifierList modifierList = psiClass.getModifierList();
-        if(modifierList == null || modifierList.hasAnnotation(LOMBOK_LOG4J2)){
+        if(modifierList == null){
             return;
         }
 
-        // Add Lombok annotation
+        if (modifierList.findAnnotation(LOMBOK_LOG4J2_FQN) != null) {
+            return;
+        }
+
         PsiElementFactory factory =  JavaPsiFacade.getElementFactory(project);
-        PsiAnnotation annotation = factory.createAnnotationFromText("@"+LOMBOK_LOG4J2, psiClass);
+
+        PsiAnnotation annotation = factory.createAnnotationFromText(
+                "@" + LOMBOK_LOG4J2_FQN,
+                psiClass
+        );
+
         modifierList.addBefore(annotation, modifierList.getFirstChild());
 
-        // Add imports and shorten class names
-        JavaCodeStyleManager.getInstance(project).shortenClassReferences(psiClass.getContainingFile());
+        JavaCodeStyleManager.getInstance(project)
+                .shortenClassReferences(annotation);
+
+        // TODO: addJavaApiLoggerField if no outside libs
+        // TODO: make addLog4jAnnotationAndImports abstract for rest of libs
+        // TODO: shorten  @lombok.extern.log4j.Log4j2, to @Log4j2
     }
 }
