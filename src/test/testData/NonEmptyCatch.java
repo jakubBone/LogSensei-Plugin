223fcabdfb2c2f1be001fffa3c012bdc371f389e
jakubBone <jakub.bone1990@gmail.com>
2025-12-15 11:21:06 +0100

    test: correct testData path and add JAVA_17 SDK to descriptor
    test: add Logger interface and LoggerFactory class as a testing mocks
    
    test: detail catch feature test with non empty catch class

--- src/test/testData/NonEmptyCatch.java
+++ src/test/testData/NonEmptyCatch.java
@@ -1,13 +1,20 @@
 
-public class CatchWithErrorLog {
-
+public class NonEmptyCatch {
     private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(CatchWithErrorLog.class);
 
-    public void method() {
+    public void withLog() {
         try {
             riskyOperation();
         } catch (Exception e) {
             log.error("Error", e);  // Log available - no action
+        }
+    }
+
+    public void withPrint() {
+        try {
+            riskyOperation();
+        } catch (Exception e) {
+            System.out.println("");  // sout available - no action
         }
     }
 
