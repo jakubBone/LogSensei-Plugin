716133f131d9ef1c7adf9dd8cb68e0fdf9f4ee79
jakubBone <jakub.bone1990@gmail.com>
2025-12-29 10:44:36 +0100

    test: add exception stubs
    test: implemented empty catch test for multi exception scenatio

--- src/test/testData/inspection/catch_block/EmptyCatch.java
+++ src/test/testData/inspection/catch_block/EmptyCatch.java
@@ -1,3 +1,5 @@
+import java.io.IOException;
+import java.sql.SQLException;
 
 public class EmptyCatch {
     public void emptyBlock() {
@@ -12,11 +14,29 @@ public class EmptyCatch {
         try {
             riskyOperation();
         } <weak_warning descr="LogSensei: Catch block missing ERROR log">catch</weak_warning> (Exception e){
-            throw new RuntimeException("IO failed", e); // Empty block - action expected
+            throw new RuntimeException("IO failed", e); // No log - action expected
         }
     }
 
+    public void withMultiException() {
+        try {
+            riskyIOOperation();
+            riskySQLOperation();
+        } <weak_warning descr="LogSensei: Catch block missing ERROR log">catch</weak_warning> (IOException | SQLException e){
+            // Empty block - action expected
+        }
+    }
+
+    // Helper
     private void riskyOperation() throws Exception {
         throw new Exception("test");
+    }
+
+    private void riskySQLOperation() throws SQLException {
+        throw new SQLException("test");
+    }
+
+    private void riskyIOOperation() throws IOException {
+        throw new IOException("test");
     }
 }
---
+++ src/test/testData/stubs/org/exception/SQLException.java
@@ -0,0 +1,11 @@
+package java.sql;
+
+public class SQLException extends Exception {
+    public SQLException() {
+        super();
+    }
+
+    public SQLException(String message) {
+        super(message);
+    }
+}
