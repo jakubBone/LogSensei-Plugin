303cb124dcef5042cb27281ded2c099f2541253b
jakubBone <jakub.bone1990@gmail.com>
2025-12-29 10:48:19 +0100

    test: implement empty return with default value scenario

--- src/test/testData/inspection/early_return/EmptyEarlyReturn.java
+++ src/test/testData/inspection/early_return/EmptyEarlyReturn.java
@@ -6,6 +6,12 @@ public class EmptyEarlyReturn {
         }
     }
 
+    public void emptyEarlyWithDefault(String var){
+        if(var == null){
+            return default; // no action
+        }
+    }
+
     public void emptyEarlyWithNoBrackets(String var){
         if(var == null) <weak_warning descr="LogSensei: Early return missing log">return</weak_warning>; // action required
     }
