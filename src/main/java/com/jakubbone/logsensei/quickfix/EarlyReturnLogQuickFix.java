5b86aa8eb53f8a8572c9e1fd4f2d59b589adbc37
jakubBone <jakub.bone1990@gmail.com>
2025-12-30 13:35:39 +0100

    refactor: isolate clean PSI logic to addLog() method in EarlyReturnLogQuickFix

--- src/main/java/com/jakubbone/logsensei/quickfix/EarlyReturnLogQuickFix.java
+++ src/main/java/com/jakubbone/logsensei/quickfix/EarlyReturnLogQuickFix.java
@@ -3,6 +3,7 @@ package com.jakubbone.logsensei.quickfix;
 import static com.jakubbone.logsensei.psi.LogImplementationService.implementLoggingSolution;
 import static com.jakubbone.logsensei.psi.LogStatementFactory.createDebugLog;
 import static com.jakubbone.logsensei.education.LogEducationNotifier.showDebugLevelEducation;
+import static com.jakubbone.logsensei.psi.LogStatementFactory.createErrorLog;
 import static com.jakubbone.logsensei.psi.PsiStatementUtils.addLogBeforeStatement;
 import static com.jakubbone.logsensei.dependency.ui.DependencyDialogService.askUserForLibraryAndAnnotation;
 
@@ -10,9 +11,12 @@ import com.intellij.codeInspection.LocalQuickFix;
 import com.intellij.codeInspection.ProblemDescriptor;
 import com.intellij.codeInspection.util.IntentionFamilyName;
 import com.intellij.openapi.project.Project;
+import com.intellij.psi.PsiCatchSection;
 import com.intellij.psi.PsiClass;
+import com.intellij.psi.PsiCodeBlock;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiMethod;
+import com.intellij.psi.PsiParameter;
 import com.intellij.psi.PsiReturnStatement;
 import com.intellij.psi.PsiStatement;
 import com.intellij.psi.util.PsiTreeUtil;
@@ -28,14 +32,16 @@ public class EarlyReturnLogQuickFix implements LocalQuickFix {
     }
 
     @Override
-    public void applyFix(@NotNull Project project, @NotNull ProblemDescriptor problemDescriptor) {
-
-        LoggingLibrary selectedLibrary = askUserForLibraryAndAnnotation(project);
-        if(selectedLibrary == null){
-            return;
+    public void applyFix(@NotNull Project project, @NotNull ProblemDescriptor descriptor) {
+        LoggingLibrary lib = askUserForLibraryAndAnnotation(project);
+        if(lib != null){
+            addLog(project, descriptor, lib);
+            showDebugLevelEducation(project);
         }
+    }
 
-        PsiElement returnKeyword = problemDescriptor.getPsiElement();
+    private void addLog(Project project, ProblemDescriptor descriptor, LoggingLibrary lib){
+        PsiElement returnKeyword = descriptor.getPsiElement();
         PsiReturnStatement returnStmt = PsiTreeUtil.getParentOfType(returnKeyword, PsiReturnStatement.class);
         if (returnStmt == null) {
             return;
@@ -51,7 +57,7 @@ public class EarlyReturnLogQuickFix implements LocalQuickFix {
             return;
         }
 
-        implementLoggingSolution(project, containingClass, selectedLibrary);
+        implementLoggingSolution(project, containingClass, lib);
 
         PsiStatement logStmt = createDebugLog(
                 project,
@@ -60,7 +66,5 @@ public class EarlyReturnLogQuickFix implements LocalQuickFix {
         );
 
         addLogBeforeStatement(project, logStmt, returnStmt);
-
-        showDebugLevelEducation(project);
     }
 }
