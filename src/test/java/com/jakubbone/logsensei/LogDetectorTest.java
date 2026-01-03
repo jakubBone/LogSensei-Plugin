3171017d1e192043baef9875ad22514337b30317
jakubBone <jakub.bone1990@gmail.com>
2026-01-03 13:54:57 +0100

    test: add LogDetector unit test

---
+++ src/test/java/com/jakubbone/logsensei/LogDetectorTest.java
@@ -0,0 +1,37 @@
+package com.jakubbone.logsensei;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import com.jakubbone.logsensei.inspection.detector.LogDetector;
+import org.junit.Test;
+
+public class LogDetectorTest {
+
+    @Test
+    public void containsLogCall_shouldDetectSlf4jPatterns() {
+        assertTrue(LogDetector.containsLogCall("log.error(\"msg\", e);"));
+        assertTrue(LogDetector.containsLogCall("log.warn(\"msg\");"));
+        assertTrue(LogDetector.containsLogCall("log.info(\"msg\");"));
+        assertTrue(LogDetector.containsLogCall("log.debug(\"msg\");"));
+    }
+
+    @Test
+    public void containsLogCall_shouldDetectLoggerVariant() {
+        assertTrue(LogDetector.containsLogCall("logger.info(\"msg\");"));
+        assertTrue(LogDetector.containsLogCall("logger.error(\"msg\");"));
+    }
+
+    @Test
+    public void containsLogCall_shouldDetectSystemOut() {
+        assertTrue(LogDetector.containsLogCall("System.out.println(\"msg\");"));
+        assertTrue(LogDetector.containsLogCall("System.out.print(x);"));
+    }
+
+    @Test
+    public void containsLogCall_shouldReturnFalse_whenNoLogPresent() {
+        assertFalse(LogDetector.containsLogCall("doSomething();"));
+        assertFalse(LogDetector.containsLogCall("String x = \"test\";"));
+        assertFalse(LogDetector.containsLogCall("return result;"));
+    }
+}
